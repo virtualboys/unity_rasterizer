3778189a9dd8b24eb4ef569158cf0d035028ba2e
many <emmanuel@archiemd.com>
2021-11-06 21:13:24 -0400

    nusceene

---
+++ Assets/Scripts/RotateChildren.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateChildren : MonoBehaviour
+{
+    [SerializeField] private float _period;
+    [SerializeField] private Vector3 _axis;
+
+    private Transform[] _children;
+    private Quaternion[] _rots;
+
+    private void Start()
+    {
+        _children = new Transform[transform.childCount];
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            _children[i] = transform.GetChild(i);
+        }
+        _rots = new Quaternion[_children.Length];
+        for(int i = 0; i < _children.Length; i++)
+        {
+            _rots[i] = _children[i].rotation;
+        }
+    }
+
+    void Update()
+    {
+        _axis.Normalize();
+        transform.rotation *= Quaternion.AngleAxis(360 * Time.deltaTime / _period, _axis);
+
+        for(int i = 0; i < _children.Length; i++)
+        {
+            _children[i].rotation = _rots[i];
+        }
+    }
+}
---
+++ Assets/Scripts/ScaleWobble.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleWobble : MonoBehaviour
+{
+    [SerializeField] private float _period;
+    [SerializeField] private float _amt;
+
+    private Vector3 _startScale;
+
+    private void Start()
+    {
+        _startScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        //transform.localScale = _startScale * 
+    }
+}
