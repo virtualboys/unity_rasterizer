63f186032ed65d6f265f15fcaf4ea8639517f394
many <emmanuel@archiemd.com>
2021-12-14 21:22:23 -0500

    leantween

--- Assets/Scripts/OffsetAnimator.cs
+++ Assets/Scripts/OffsetAnimator.cs
@@ -12,6 +12,7 @@ public class OffsetAnimation
 
     public bool Oscillate;
     public bool Random;
+    public LeanTweenType EaseType = LeanTweenType.easeInOutQuad;
     [Range(-2000, 2000)]
     public float Value;
     public float Period;
@@ -20,30 +21,70 @@ public class OffsetAnimation
     [Range(-1024, 1024)]
     public float Select;
 
-    public OSC OSCSender;
-    public float OSCMaxValue = 1;
+    //public OSC OSCSender;
+    //public float OSCMaxValue = 1;
 
-    public OffsetOverride OffsetOverride;
+    //public OffsetOverride OffsetOverride;
+
+    private int _tweenId;
 
     public void Update()
     {
         if(Oscillate)
         {
-            Value = (1024 * Mathf.Sin(2.0f * Mathf.PI * Time.time / Period));
+            if(Random)
+            {
+                if(_tweenId == -1)
+                {
+                    RandomTween();
+                }
+            }
+            else
+            {
+                if(_tweenId != -1)
+                {
+                    LeanTween.cancel(_tweenId);
+                    _tweenId = -1;
+                }
+                Value = (1024 * Mathf.Sin(2.0f * Mathf.PI * Time.time / Period));
+            }
         }
-        if(Mathf.Abs(Value) > 7800)
+        else if(_tweenId != -1)
+        {
+            LeanTween.cancel(_tweenId);
+            _tweenId = -1;
+        }
+        
+        if(Mathf.Abs(Value) > 1800)
         {
             Value = float.NaN;
         }
+    }
+
+    private void RandomTween()
+    {
+        _tweenId = LeanTween.value(OffsetAnimator.AnimatorObject, UpdateValue, Value, UnityEngine.Random.Range(-1024, 1024), Period + Period * UnityEngine.Random.value).setOnComplete(RandomTween).id;
+    }
+
+    private void UpdateValue(float val)
+    {
+        Value = val;
     }
 }
 
 public class OffsetAnimator : MonoBehaviour
 {
+    public static GameObject AnimatorObject;
+
     [SerializeField] private AnalogInputManager _inputManager;
 
     [SerializeField] private OffsetAnimation[] _jackOscillationPeriods;
     [SerializeField] private OffsetAnimation _selectOscillation;
+
+    private void Awake()
+    {
+        AnimatorObject = gameObject;
+    }
 
     private void Update()
     {
@@ -59,10 +100,10 @@ public class OffsetAnimator : MonoBehaviour
     {
         o.Update();
 
-        if (o.OffsetOverride != null)
-        {
-            o.Value = o.OffsetOverride.Offset;
-        }
+        //if (o.OffsetOverride != null)
+        //{
+        //    o.Value = o.OffsetOverride.Offset;
+        //}
 
         if (o.SelectOverride)
         {
@@ -75,13 +116,13 @@ public class OffsetAnimator : MonoBehaviour
 
         o.InputDescription = _inputManager.GetCurrentDescription(jackInd);
 
-        if (o.OSCSender != null)
-        {
-            OscMessage message = new OscMessage();
-            message.address = o.InputDescription.CurrentParameter;
-            float value = o.OSCMaxValue * (.5f + o.Value / 2048);
-            message.values.Add(value);
-            o.OSCSender.Send(message);
-        }
+        //if (o.OSCSender != null)
+        //{
+        //    OscMessage message = new OscMessage();
+        //    message.address = o.InputDescription.CurrentParameter;
+        //    float value = o.OSCMaxValue * (.5f + o.Value / 2048);
+        //    message.values.Add(value);
+        //    o.OSCSender.Send(message);
+        //}
     }
 }
